dfe664b9f3da4a6b2f385c047e71dc8d0a7a86e5
Anton <anton.tseniukh@gmail.com>
2021-12-05 12:59:49 +0200

    add corrections

--- src/main/resources/projectdb.sql
+++ src/main/resources/projectdb.sql
@@ -1,12 +1,12 @@
+USE projectdb;
 CREATE TABLE IF NOT EXISTS user (id int auto_increment, name text(255), email varchar(255), UNIQUE(email), primary key(id));
-
 TRUNCATE TABLE user;
-
 DROP TABLE user;
-
 SELECT* FROM user;
-
 INSERT INTO user
 (name, email)
 VALUES
 ('Anton', 'anton.tseniukh@gmail.com');
+UPDATE user SET name="Anton", email="anton.tseniukh@gmail.com" WHERE id=1;
+SELECT* FROM user WHERE id=1;
+DELETE FROM user WHERE id=2;
