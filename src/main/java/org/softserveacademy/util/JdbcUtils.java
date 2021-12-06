08b746f07947cd04ab388d91913b348917de0ad8
Anton <anton.tseniukh@gmail.com>
2021-12-06 22:13:33 +0200

    add connection repair

--- src/main/java/org/softserveacademy/util/JdbcUtils.java
+++ src/main/java/org/softserveacademy/util/JdbcUtils.java
@@ -1,5 +1,8 @@
 package org.softserveacademy.util;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,25 +10,27 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class JdbcUtils {
+    private static final String dbPropertiesFile = ".\\src\\main\\resources\\application.properties";
     private static final Properties properties = new Properties();
-    private static Connection connection;
 
-    private static Connection getConnection() {
-        Connection con = null;
+    private static Connection getConnection() throws IOException {
+        Connection connection = null;
         try {
+            FileReader reader = new FileReader(dbPropertiesFile);
+            properties.load(reader);
             Class.forName(properties.getProperty("com.mysql.jdbc.Driver"));
-            con = DriverManager.getConnection(properties.getProperty("database.url"), properties.getProperty("database.user")
+            connection = DriverManager.getConnection(properties.getProperty("database.url"), properties.getProperty("database.user")
                     , properties.getProperty("database.pass"));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
-        return con;
+        return connection;
     }
 
     public static PreparedStatement getPreparedStatement(String sql) {
         try {
             return getConnection().prepareStatement(sql);
-        } catch (SQLException t) {
+        } catch (SQLException | IOException t) {
             throw new IllegalArgumentException("");
         }
     }
