4329a0783cce2377bca1a4bce775667541104720
Anton <anton.tseniukh@gmail.com>
2021-12-20 23:36:36 +0200

    add corrections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import org.softserveacademy.view.UserView;
 import org.softserveacademy.view.ViewImpl;
 
 
 public class Main {
     public static void main(String[] args) {
-        ViewImpl viewImpl = new ViewImpl();
-        viewImpl.start();
+//        ViewImpl viewImpl = new ViewImpl();
+//        viewImpl.start();
+        UserView userView = new UserView();
     }
 }
--- src/main/java/org/softserveacademy/servlet/UserServlet.java
+++ src/main/java/org/softserveacademy/servlet/UserServlet.java
@@ -1,29 +1,25 @@
 package org.softserveacademy.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.softserveacademy.model.User;
-import org.softserveacademy.repository.UserRepository;
 import org.softserveacademy.repository.jdbc.JdbcUserRepositoryImpl;
-import org.softserveacademy.services.impl.UserServiceImpl;
+import org.softserveacademy.view.UserView;
 
 import java.io.IOException;
-import java.util.List;
 
 
 //@WebServlet(name = "UserServlet")
 public class UserServlet extends HttpServlet {
-    private JdbcUserRepositoryImpl jdbcUserRepository;
-
-    @Override
-    public void init() {
-        jdbcUserRepository = new JdbcUserRepositoryImpl();
-    }
+    //private final JdbcUserRepositoryImpl jdbcUserRepository = new JdbcUserRepositoryImpl();
+    UserView userView = new UserView();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //new UserRepository().getById()
-        User user = jdbcUserRepository.getById((Integer) request.getSession().getAttribute("id"));
+        //User user = jdbcUserRepository.getById((Integer) request.getSession().getAttribute("id"));
+        User user = userView.getUserFromController();
         response.setContentType("application/json");
         response.getWriter().write(new ObjectMapper().writeValueAsString(user));
     }
@@ -32,11 +28,7 @@ public class UserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         User user = new User(request.getParameter("name"), request.getParameter("email"));
         System.out.println(user);
-        jdbcUserRepository.save(user);
-    }
-
-    @Override
-    public void destroy() {
-
+        //jdbcUserRepository.save(user);
+        userView.createUserFromController();
     }
 }
